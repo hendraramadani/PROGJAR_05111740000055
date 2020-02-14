29cf1cb1ae2fc32d29248df6237aca712947c072
Hendra Ramadani <hendraramadani14@gmail.com>
2020-02-15 00:27:06 +0700

    just try

--- tugas1b/client/client.py
+++ tugas1b/client/client.py
@@ -9,14 +9,26 @@ server_address = ('localhost', 9998)
 print('connecting to %s port %s' % server_address , file=sys.stderr)
 sock.connect(server_address)
 
-file = open('HelloClient.jpg','rb')
-file_send = file.read(1024)
+file = open('HelloClient.txt','wb')
 
-try:
-    sock.sendall(file_send)
-    fl_send = file.read(1024)
-    print('sending file to server',file=sys.stderr)
-finally:
-    print("closing")
+# try:
+#     # sock.sendall(file_send)
+#     # fl_send = file.read(1024)
+#     # print('sending file to server',file=sys.stderr)
+message = 'request file'
+sock.sendall(message.encode())
+# finally:
+#     print("closing")
+#     sock.shutdown(2)
+#     sock.close()
+while True:
+    file_receive = sock.recv(1024)
+    # print(file_receive)
+    while file_receive:
+        file.write(file_receive)
+        file_receive = sock.recv(1024)
+        print('file from server received' , file=sys.stderr)
+    file.close()
     sock.shutdown(2)
     sock.close()
+    break
--- tugas1b/server/server.py
+++ tugas1b/server/server.py
@@ -8,22 +8,32 @@ print('starting up on %s port %s' % server_address , file=sys.stderr)
 sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
-file = open('HelloServer.jpg','wb')
+file = open('HelloServer.txt','rb')
+file_send = file.read(1024)
 while True:
 	# Wait for a connection
 	print('waiting for a connection' , file=sys.stderr)
 	connection, client_address = sock.accept()
 	print('connection from', client_address , file=sys.stderr)
 	# Receive the data in small chunks and retransmit it
-	file_receive=connection.recv(1024)
-	while file_receive:
-		file.write(file_receive)
-		file_receive = connection.recv(1024)
-		print('file from client received' , file=sys.stderr)
-	# Clean up the connection
-	file.close
-	connection.shutdown(2)
-	connection.close()
-	sock.close()
-	break
-
+	# file_receive=connection.recv(1024)
+	# while file_receive:
+	# 	file.write(file_receive)
+	# 	file_receive = connection.recv(1024)
+	# 	print('file from client received' , file=sys.stderr)
+	# # Clean up the connection
+	# file.close
+	# connection.shutdown(2)
+	# connection.close()
+	data = connection.recv(32).decode()
+	print ('client '+data)
+	if data == "request file":
+		print('ok')
+		try:
+			connection.send(file_send)
+			fl_send = file.read(1024)
+			print('sending file to client',file=sys.stderr)
+		finally:
+			print("closing")
+			sock.close()
+			break
