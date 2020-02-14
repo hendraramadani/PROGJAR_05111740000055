aee575393877027d7e454f0b9460abd4d7d6d14d
Hendra Ramadani <hendraramadani14@gmail.com>
2020-02-14 21:50:50 +0700

    proses

--- tugas1a/client/base64.py
+++
@@ -1,6 +0,0 @@
-import sys
-import base64
-
-with open('HelloClient.jpg', 'rb') as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-    image_encoded = encoded_string.decode('utf-8')
---
+++ tugas1b/client/client.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 9998)
+print('connecting to %s port %s' % server_address , file=sys.stderr)
+sock.connect(server_address)
+
+file = open('HelloClient.jpg','rb')
+file_send = file.read(1024)
+
+try:
+    sock.sendall(file_send)
+    fl_send = file.read(1024)
+    print('sending file to server',file=sys.stderr)
+finally:
+    print("closing")
+    sock.shutdown(2)
+    sock.close()
